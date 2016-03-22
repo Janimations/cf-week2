aea685b33e9b6300efcfb16810aec19d921aa178
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-22 13:00:30 -0700

    Started creating object store1

--- pizza3001/js/app.js
+++ pizza3001/js/app.js
@@ -2,17 +2,16 @@ var exports = {};
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  // var store1 = {
-  //   name: 'Beaverton',
-  //   deliveryData: function() {
-  //     for (ii=0; ii<marketData.length; ii++) {
-  //
-  //     }
-  //   }
-  // };
-
-  pizzasDuringHour(7);
-  deliveriesDuringHour(7);
-  driversDuringHour(7);
+var store1 = {
+  name: 'Beaverton',
+  deliveryData: function() {
+    for (ii=0; ii<marketData.length; ii++) {
+      var time = marketData[ii].time;
+      var pizzasThisHour = pizzasDuringHour(ii);
+      var deliveriesThisHour = deliveriesDuringHour(ii);
+      var driversThisHour = driversDuringHour(ii);
+    }
+  }
+};
 
 });
