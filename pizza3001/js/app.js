8857a248b9b8376443c31b695eb9e3d6cdaab267
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-22 13:15:26 -0700

    Created objects for other locations

--- pizza3001/js/app.js
+++ pizza3001/js/app.js
@@ -16,12 +16,77 @@ var store1 = {
   }
 };
 
+var store2 = {
+  name: 'Hillsboro',
+  deliveryData: function() {
+    for (ii=0; ii<marketData.length; ii++) {
+      var time = marketData[ii].time;
+      var pizzasThisHour = pizzasDuringHour(ii);
+      var deliveriesThisHour = deliveriesDuringHour(ii);
+      var driversThisHour = driversDuringHour(ii);
+      var message = time + ': ' + pizzasThisHour + ' pizzas, ' + deliveriesThisHour + ' deliveries -- drivers recommended: ' + driversThisHour + '.';
+      console.log(message);
+    }
+  }
+};
+
+var store3 = {
+  name: 'Downtown',
+  deliveryData: function() {
+    for (ii=0; ii<marketData.length; ii++) {
+      var time = marketData[ii].time;
+      var pizzasThisHour = pizzasDuringHour(ii);
+      var deliveriesThisHour = deliveriesDuringHour(ii);
+      var driversThisHour = driversDuringHour(ii);
+      var message = time + ': ' + pizzasThisHour + ' pizzas, ' + deliveriesThisHour + ' deliveries -- drivers recommended: ' + driversThisHour + '.';
+      console.log(message);
+    }
+  }
+};
+
+var store4 = {
+  name: 'Northeast',
+  deliveryData: function() {
+    for (ii=0; ii<marketData.length; ii++) {
+      var time = marketData[ii].time;
+      var pizzasThisHour = pizzasDuringHour(ii);
+      var deliveriesThisHour = deliveriesDuringHour(ii);
+      var driversThisHour = driversDuringHour(ii);
+      var message = time + ': ' + pizzasThisHour + ' pizzas, ' + deliveriesThisHour + ' deliveries -- drivers recommended: ' + driversThisHour + '.';
+      console.log(message);
+    }
+  }
+};
+
+var store5 = {
+  name: 'Clackamas',
+  deliveryData: function() {
+    for (ii=0; ii<marketData.length; ii++) {
+      var time = marketData[ii].time;
+      var pizzasThisHour = pizzasDuringHour(ii);
+      var deliveriesThisHour = deliveriesDuringHour(ii);
+      var driversThisHour = driversDuringHour(ii);
+      var message = time + ': ' + pizzasThisHour + ' pizzas, ' + deliveriesThisHour + ' deliveries -- drivers recommended: ' + driversThisHour + '.';
+      console.log(message);
+    }
+  }
+};
+
+var store6 = {
+  name: 'PDX-Airport',
+  deliveryData: function() {
+    for (ii=0; ii<marketData.length; ii++) {
+      var time = marketData[ii].time;
+      var pizzasThisHour = pizzasDuringHour(ii);
+      var deliveriesThisHour = deliveriesDuringHour(ii);
+      var driversThisHour = driversDuringHour(ii);
+      var message = time + ': ' + pizzasThisHour + ' pizzas, ' + deliveriesThisHour + ' deliveries -- drivers recommended: ' + driversThisHour + '.';
+      console.log(message);
+    }
+  }
+};
+
 console.log(store1.name);
 store1.deliveryData();
-
-//For testing functions
-// pizzasDuringHour(7);
-// deliveriesDuringHour(7);
-// driversDuringHour(7);
 
 });
