a8c524c1e4bcc37587d16daf551648cb7b8eb559
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-22 15:19:04 -0700

    Finished createPizzaTable function. Starting function to populate pizza table.

--- pizza3001/js/app.js
+++ pizza3001/js/app.js
@@ -86,18 +86,40 @@ var store6 = {
   }
 };
 
-function createTable(title, colHead1, colHead2, colHead3, colHead4, id) {
-  var newTable = document.createElement(table);
-  var 
+function createPizzaTable(title, id) {
+  var newTable = document.createElement('table');
+  newTable.setAttribute('id', id);
+  var bodyEl = document.getElementsByTagName('body')[0];
+  bodyEl.appendChild(newTable);
+  var newTableRow = document.createElement('tr');
+  newTable.appendChild(newTableRow);
+
+  var headings = ['Hour', 'Pizzas', 'Deliveries', 'Drivers'];
+
+  for (jj=0; jj<headings.length; jj++) {
+    var tableHeading = document.createElement('th');
+    newTableRow.appendChild(tableHeading);
+    var headingText = document.createTextNode(headings[jj]);
+    tableHeading.appendChild(headingText);
+  }
 }
 
-function createAndPlaceEl(element, text, parentEl, position) {
-  var newEl = document.createElement(element);
-  var newText = document.createTextNode(text);
-  newEl.appendChild(newText);
-  var position = document.getElementsByTagName(parentEl)[position];
-  position.appendChild(newEl);
-}
+var getBody = document.getElementsByTagName('body')[0];
+var h2new = document.createElement('h2');
+var h2text = document.createTextNode(__LOCATION__);
+h2new.appendChild(h2text);
+getBody.appendChild(h2new);
+
+
+createPizzaTable('BEAVERTON', 'beaverton');
+
+// function createAndPlaceEl(element, text, parentEl, position) {
+//   var newEl = document.createElement(element);
+//   var newText = document.createTextNode(text);
+//   newEl.appendChild(newText);
+//   var position = document.getElementsByTagName(parentEl)[position];
+//   position.appendChild(newEl);
+// }
 
 //CREATE FUNCTION TO CREATE ELEMENT AND PLACE IT IN DOM TREE. ADD FOR LOOP TO LOOP THROUGH STORES AND USE THAT FUNCTION TO PRINT NAMES AND DELIVERY DATA (message VARIABLE IN ABOVE OBJECTS) TO PAGE IN LIST FORM. - DUCKETT P. 223!!!!
 
