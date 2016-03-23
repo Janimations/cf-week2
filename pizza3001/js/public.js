9f4e429f70baa04fbd3f9cbd1b937da05e492275
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-23 11:53:50 -0700

    Defined function getTotalPizzasPerWeek and printed tppw to index.html. Made adjustments to styles.

---
+++ pizza3001/js/public.js
@@ -0,0 +1,8 @@
+// (function() {
+
+getTotalPizzasPerWeek();
+var counterEl = document.getElementById("pizza-counter");
+counterEl.textContent = totalPizzasPerWeek;
+console.log(totalPizzasPerWeek);
+
+// });
