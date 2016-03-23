e2dbd469f7c474756a9f6de331622b9361321f9f
Taylor Zelaya <taylormzelaya@gmail.com>
2016-03-22 21:45:36 -0700

    Finalized createPizzaTable method for store6; to be copied to other store objects.

--- pizza3001/js/app.js
+++ pizza3001/js/app.js
@@ -74,61 +74,53 @@ var store5 = {
 
 var store6 = {
   name: 'PDX-Airport',
-  deliveryData: function() {
+  createPizzaTable: function(title, tableId) {
+    console.log(title + tableId);
+    //Create h2 and first row and insert both into page
+    var bodyEl = document.getElementsByTagName('body')[0];
+    var h2new = document.createElement('h2');
+    var h2text = document.createTextNode(title);
+    h2new.appendChild(h2text);
+    bodyEl.appendChild(h2new);
+    var newTable = document.createElement('table');
+    newTable.setAttribute('id', tableId);
+    bodyEl.appendChild(newTable);
+    var newTableRow = document.createElement('tr');
+    newTable.appendChild(newTableRow);
+
+    var headings = ['Hour', 'Pizzas', 'Deliveries', 'Drivers'];
+
+    for (jj=0; jj<headings.length; jj++) {
+      var tableHeading = document.createElement('th');
+      newTableRow.appendChild(tableHeading);
+      var headingText = document.createTextNode(headings[jj]);
+      tableHeading.appendChild(headingText);
+    }
+
+    var pizzaTable = document.getElementById(tableId);
+
     for (ii=0; ii<marketData.length; ii++) {
+      //Generate random data from marketData
       var time = marketData[ii].time;
       var pizzasThisHour = pizzasDuringHour(ii);
       var deliveriesThisHour = deliveriesDuringHour(ii);
       var driversThisHour = driversDuringHour(ii);
-      var message = time + ': ' + pizzasThisHour + ' pizzas, ' + deliveriesThisHour + ' deliveries -- drivers recommended: ' + driversThisHour + '.';
-      console.log(message);
+      //store random data in array
+      var thisHourInfo = [time, pizzasThisHour, deliveriesThisHour, driversThisHour];
+      //Make new row for each hour's random data array
+      var newTableRow = document.createElement('tr');
+      for (kk=0; kk<thisHourInfo.length; kk++) {
+        var newTableCell = document.createElement('td');
+        var tdText = document.createTextNode(thisHourInfo[kk]);
+        newTableCell.appendChild(tdText);
+        newTableRow.appendChild(newTableCell);
+      }
+      //Append each hour's row to pizzaTable
+      pizzaTable.appendChild(newTableRow);
     }
   }
 };
 
-function createPizzaTable(title, tableId) {
-  var bodyEl = document.getElementsByTagName('body')[0];
-  var h2new = document.createElement('h2');
-  var h2text = document.createTextNode(title);
-  h2new.appendChild(h2text);
-  bodyEl.appendChild(h2new);
-  var newTable = document.createElement('table');
-  newTable.setAttribute('id', tableId);
-  bodyEl.appendChild(newTable);
-  var newTableRow = document.createElement('tr');
-  newTable.appendChild(newTableRow);
-
-  var headings = ['Hour', 'Pizzas', 'Deliveries', 'Drivers'];
-
-  for (jj=0; jj<headings.length; jj++) {
-    var tableHeading = document.createElement('th');
-    newTableRow.appendChild(tableHeading);
-    var headingText = document.createTextNode(headings[jj]);
-    tableHeading.appendChild(headingText);
-  }
-}
-
-function createTableRow(tableId, thisRowId, array) {  //array of td's
-  for (kk=0; kk<array.length; kk++) {
-    var newTableRow = document.createElement('tr');
-    newTableRow.setAttribute('id', thisRowId);
-    var table = document.getElementById(tableId);
-    table.appendChild(newTableRow);
-}
-
-createPizzaTable('BEAVERTON', 'beaverton');
-
-// function createAndPlaceEl(element, text, parentEl, position) {
-//   var newEl = document.createElement(element);
-//   var newText = document.createTextNode(text);
-//   newEl.appendChild(newText);
-//   var position = document.getElementsByTagName(parentEl)[position];
-//   position.appendChild(newEl);
-// }
-
-//CREATE FUNCTION TO CREATE ELEMENT AND PLACE IT IN DOM TREE. ADD FOR LOOP TO LOOP THROUGH STORES AND USE THAT FUNCTION TO PRINT NAMES AND DELIVERY DATA (message VARIABLE IN ABOVE OBJECTS) TO PAGE IN LIST FORM. - DUCKETT P. 223!!!!
-
-console.log(store1.name);
-store1.deliveryData();
+store6.createPizzaTable('PDX-Airport', 'pdx-airport');
 
 });
